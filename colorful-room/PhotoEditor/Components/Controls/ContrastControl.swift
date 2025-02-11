fdf29d77edda9eb463d8668cf1eace1c78461cdc
Bugzer0 <lexuanchinh87@gmail.com>
2025-02-11 15:22:04 +0700

    update contrast to brightrroom

--- colorful-room/PhotoEditor/Components/Controls/ContrastControl.swift
+++ colorful-room/PhotoEditor/Components/Controls/ContrastControl.swift
@@ -1,51 +1,61 @@
-//
-//  ContrastControl.swift
-//  colorful-room
-//
-//  Created by macOS on 7/13/20.
-//  Copyright © 2020 PingAK9. All rights reserved.
-//
-
 import SwiftUI
-import PixelEnginePackage
+import BrightroomEngine
+import BrightroomUI
 
 struct ContrastControl: View {
-    @State var filterIntensity:Double = 0
+    @State var filterIntensity: Double = 0
+    @EnvironmentObject var shared: PECtl
     
     var body: some View {
-        
         let intensity = Binding<Double>(
             get: {
                 self.filterIntensity
-        },
+            },
             set: {
                 self.filterIntensity = $0
                 self.valueChanged()
-        }
+            }
         )
-        let min = FilterContrast.range.min
-        let max = FilterContrast.range.max
-        return  FilterSlider(value: intensity, range: (min, max), defaultValue: 0)
-        .onAppear(perform: didReceiveCurrentEdit)
+        
+        return FilterSlider(value: intensity,
+                          range: (-1.0, 1.0),
+                          defaultValue: 0)
+            .onAppear(perform: didReceiveCurrentEdit)
     }
     
     func didReceiveCurrentEdit() {
-        
-        let edit: EditingStack.Edit = PECtl.shared.editState.currentEdit
-        self.filterIntensity = edit.filters.contrast?.value ?? 0
+        if let stack = shared.brightroomEditingStack {
+            if let loadedState = stack.store.state.loadedState {
+                self.filterIntensity = loadedState.currentEdit.filters.contrast?.value ?? 0
+            }
+        }
     }
     
     func valueChanged() {
-        
         let value = self.filterIntensity
         
-        guard value != 0 else {
-            PECtl.shared.didReceive(action: PECtlAction.setFilter({ $0.contrast = nil }))
-            return
+        if let stack = shared.brightroomEditingStack {
+            if value == 0 {
+                stack.set(filters: {
+                    $0.contrast = nil
+                })
+                PECtl.shared.didReceive(action: PECtlAction.setFilter({ $0.contrast = nil }))
+            } else {
+                // Cập nhật cho BrightroomEngine
+                stack.set(filters: {
+                    var filter = BrightroomEngine.FilterContrast()
+                    filter.value = value
+                    $0.contrast = filter
+                })
+                
+                // Cập nhật cho preview thông qua PECtl
+                PECtl.shared.didReceive(action: PECtlAction.setFilter({ filters in
+                    if filters.contrast == nil {
+                        filters.contrast = .init()
+                    }
+                    filters.contrast?.value = value
+                }))
+            }
         }
-        
-        var f = FilterContrast()
-        f.value = value
-        PECtl.shared.didReceive(action: PECtlAction.setFilter({ $0.contrast = f }))
     }
 }
